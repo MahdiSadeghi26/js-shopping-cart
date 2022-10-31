0daa3735e6a791cb09623a66aba34071ddb0d90c
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2022-10-31 19:39:19 +0330

    item generator elms added part1

--- js/store.js
+++ js/store.js
@@ -4,21 +4,36 @@ const itemContainer = $.querySelector(".cart-items");
 const addBtn=$.querySelectorAll('.shop-item-button')
 // funciton
 function addToCart(e) {
-    let item;
-    addBtn.forEach(element => {
-        element.addEventListener('click',(e)=>{
-            item={
+    let itemObj;
+    addBtn.forEach(item => {
+        item.addEventListener('click',(e)=>{
+            itemObj={
                 itemImage:e.target.parentElement.parentElement.children[1],
                 itemTitle:e.target.parentElement.parentElement.children[0].innerHTML,
                 itemPrice:e.target.previousElementSibling.innerHTML,
-                itemCount:1
             }
             
         })
     });
 }
 function itemGenerator(item) {
+    let rowDiv=$.createElement('div')
+        rowDiv.classList.add(cart-row)
+    let infoDev=$.createElement('div')
+        infoDev.classList.add('cart-item','cart-column')
     
+        let itemImage=$.createElement('img')
+        itemImage.classList.add('cart-item-image')
+    let priceSpan=$.createElement('span')
+        priceSpan.classList.add('cart-price','cart-column')
+    let countDiv=$.createElement('div')
+        countDiv.classList.add('cart-quantity' ,'cart-column')
+    let inputNumber=$.createElement('input')
+        inputNumber.setAttribute=('type','number')
+    let removebtn=$.createElement('button')
+        removebtn.classList.add('btn','btn-danger')
+        removebtn.innerHTML='Remove'
+    rowDiv.append(infoDev,priceSpan,countDiv)
 }
 // event listener
 window.addEventListener('load',addToCart)
